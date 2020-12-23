9b69baa296fb01246e65ac469856aede9b686890
vindarel <vindarel@mailz.org>
2020-12-23 23:26:34 +0100

    get user and project as CLI args

---
+++ README.md
@@ -0,0 +1,18 @@
+
+View the last Gitlab pipelines and their status.
+
+Really not much here! Except it does the job \o/
+
+```
+$ pipelines-viewer vindarel abelujo
+OK  2020-12-23T21:44:14.261Z
+OK  2020-12-23T21:20:41.078Z
+FAILED  2020-12-23T20:50:23.998Z
+skipped  2020-12-23T20:21:29.997Z
+OK  2020-12-23T20:20:23.708Z
+skipped  2020-12-23T20:06:43.823Z
+skipped  2020-12-23T19:37:06.454Z
+OK  2020-12-23T17:57:24.995Z
+skipped  2020-12-22T14:59:15.645Z
+OK  2020-12-08T18:18:36.785Z
+```
--- src/pipelines-viewer.lisp
+++ src/pipelines-viewer.lisp
@@ -23,17 +23,43 @@
   (string-equal (access:access pipeline :|status|)
                 "success"))
 
+
+(defun is-error (pipeline)
+  (string-equal (access:access pipeline :|status|)
+                "failed"))
+
 (defun show-last-pipelines (data)
-  (loop for item in (str:substring 0 5 data)
+  (loop for item in (str:substring 0 10 data)
      do (cond
           ((is-success item)
            (format t "~a~t ~a~&"
                    (cl-ansi-text:green "OK")
+                   (access:access item :|updated_at|)))
+          ((is-error item)
+           (format t "~a~t ~a~&"
+                   (cl-ansi-text:red "FAILED")
+                   (access:access item :|updated_at|)))
+          (t
+           (format t "~a~t ~a~&"
+                   (access:access item :|status|)
                    (access:access item :|updated_at|))))))
 
 #+(or nil)
 (get-pipelines "vindarel%2Fabelujo")
 
+(defun get-project-remote-url ()
+  (let ((config (str:from-file ".git/config")))
+    (cond
+      ;; also doable with a call to git remotes -v
+      ((not (str:containsp "[remote" config))
+       (format nil "Does this repository has a remote?"))
+      (t
+       (error "todo :)")))))
+
 (defun main ()
-  (let ((project-id "vindarel%2Fabelujo"))
+  (unless (uiop:command-line-arguments)
+    (format *error-output* "Usage: pipelines-viewer username project")
+    (uiop:quit 1))
+  (let* ((args (uiop:command-line-arguments))
+         (project-id (format nil "~a%2F~a" (first args) (second args))))
     (show-last-pipelines (get-pipelines project-id))))
